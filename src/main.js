2537e2b93a2a7bdc5f3f293a138f14c4a4da7c92
Hwajiin <borderline0px@gmail.com>
2021-11-17 19:43:01 +0900

    🍱 Import parallax and bg images

--- src/main.js
+++ src/main.js
@@ -2,3 +2,5 @@ import "./main.scss";
 import "./assets/images/mountain-01.jpg";
 import "./assets/images/mountain-02.jpg";
 import "./assets/images/mountain-03.jpg";
+import "./assets/images/mountain-bg-02.jpg";
+import "./assets/images/mountain-parallax-01.png";
