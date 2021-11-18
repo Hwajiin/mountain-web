a0b615ee96818becf41b24fc50419f4e916cbf59
Hwajiin <borderline0px@gmail.com>
2021-11-18 23:01:28 +0900

    📝 Add import scroll.js and menu-open.js files

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import "./main.scss";
+import "./js/scroll";
+import "./js/menu-open";
+
 import "./assets/images/mountain-01.jpg";
 import "./assets/images/mountain-02.jpg";
 import "./assets/images/mountain-03.jpg";
