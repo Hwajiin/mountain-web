dc85c58daff4bd202be89cdd33234f27ba8ddbcd
Hwajiin <borderline0px@gmail.com>
2021-11-20 13:33:14 +0900

    🔥 Remove parallax img file

--- src/main.js
+++ src/main.js
@@ -6,5 +6,4 @@ import "./assets/images/mountain-01.jpg";
 import "./assets/images/mountain-02.jpg";
 import "./assets/images/mountain-03.jpg";
 import "./assets/images/mountain-bg-02.jpg";
-import "./assets/images/mountain-parallax-01.png";
 import "./assets/images/main-thumbnail.jpg";
