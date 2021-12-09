442fb26ea5035dc9862b8083dbad541b090ace79
Hwajiin <borderline0px@gmail.com>
2021-12-09 13:42:43 +0900

    🍱 Import responsive images

--- src/main.js
+++ src/main.js
@@ -7,3 +7,6 @@ import "./assets/images/mountain-02.jpg";
 import "./assets/images/mountain-03.jpg";
 import "./assets/images/mountain-bg-02.jpg";
 import "./assets/images/main-thumbnail.jpg";
+import "./assets/images/mountain-bg-02_raqz8i_c_scale,w_792.jpg";
+import "./assets/images/mountain-bg-02_raqz8i_c_scale,w_987.jpg";
+import "./assets/images/mountain-bg-02_raqz8i_c_scale,w_1400.jpg";
